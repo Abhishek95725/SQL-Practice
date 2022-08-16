52793987d57524cc84072b6a13227f28253e711c
Abhishek Thakur <75131439+Abhishek95725@users.noreply.github.com>
2022-08-17 01:17:58 +0530

    NotNull
    In this file only NOT NULL query. 
    NOT NULL : NOT NULL means you cannot leave any table column as NULL . 
    * (Not Null means kisi bhi Table ke column ko Null nahi chhod sakte)

---
+++ NotNull.sql
@@ -0,0 +1,22 @@
+use Abhishek
+-- NOT NULL Practice
+create table test(Id int NOT NULL, 
+                  First_Name varchar (20), 
+                  Last_Name varchar (20), 
+                  City varchar(50), 
+                  College varchar(30),
+                  Salary int)
+insert into test values(101, 'Abhishek', 'Thakur', 'Champaran', 'SNS College', 95000)
+insert into test values(102, 'Chris', 'Hemsworth', 'Australia', 'Hollywood College', 55600)
+insert into test values(103, 'Tom', 'Cruiz', 'California', 'California Boys College', 65000)
+insert into test values(null, 'Tom', 'Thor', 'Motihari', 'MTH College', 45000)
+
+
+select * from test
+drop table test
+
+alter table test alter column city varchar(50) NOT NULL
+insert into test values(104, 'Alex', 'Lucy', null, 'NIT College', 88000)
+alter table test alter column salary int null
+
+sp_help test
